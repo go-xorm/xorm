89da5db77fa7ad9721394cb8d00f6d7b2320776f
Lunny Xiao <xiaolunwen@gmail.com>
2013-07-03 11:54:27 +0800

    fix test passwd

--- mysql_test.go
+++ mysql_test.go
@@ -14,7 +14,7 @@ utf8 COLLATE utf8_general_ci;
 
 func TestMysql(t *testing.T) {
 	// You should drop all tables before executing this testing
-	me = Create("mysql", "root:123@/test?charset=utf8")
+	me = Create("mysql", "root:@/orm_test?charset=utf8")
 	me.ShowSQL = true
 
 	directCreateTable(&me, t)
