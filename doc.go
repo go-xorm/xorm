1baec0ce1ad71381f368ae2953258c152145d82d
Lunny Xiao <xiaolunwen@gmail.com>
2013-10-07 11:15:24 +0800

    improved doc.go

--- doc.go
+++ doc.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD
 // license that can be found in the LICENSE file.
 
-// Package xorm provides is a simple and powerful ORM for Go. It makes your
+// Package xorm provides is a simple and powerful ORM for Go. It makes
 // database operation simple.
-
 package xorm
