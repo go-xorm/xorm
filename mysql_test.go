b71b3f0ad3e676c4d15064cf3a751790c375c439
Nash Tsai <nash.tsai@gmail.com>
2013-12-06 11:09:34 +0800

    add mysqlDdlImport to mysql_test.go

--- mysql_test.go
+++ mysql_test.go
@@ -10,23 +10,37 @@ CREATE DATABASE IF NOT EXISTS xorm_test CHARACTER SET
 utf8 COLLATE utf8_general_ci;
 */
 
+var mysqlShowTestSql bool = true
+
 func TestMysql(t *testing.T) {
+	err := mysqlDdlImport()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
 	engine, err := NewEngine("mysql", "root:@/xorm_test?charset=utf8")
 	defer engine.Close()
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	engine.ShowSQL = showTestSql
-	engine.ShowErr = showTestSql
-	engine.ShowWarn = showTestSql
-	engine.ShowDebug = showTestSql
+	engine.ShowSQL = mysqlShowTestSql
+	engine.ShowErr = mysqlShowTestSql
+	engine.ShowWarn = mysqlShowTestSql
+	engine.ShowDebug = mysqlShowTestSql
 
 	testAll(engine, t)
 	testAll2(engine, t)
 }
 
 func TestMysqlWithCache(t *testing.T) {
+	err := mysqlDdlImport()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
 	engine, err := NewEngine("mysql", "root:@/xorm_test2?charset=utf8")
 	defer engine.Close()
 	if err != nil {
@@ -34,13 +48,29 @@ func TestMysqlWithCache(t *testing.T) {
 		return
 	}
 	engine.SetDefaultCacher(NewLRUCacher(NewMemoryStore(), 1000))
-	engine.ShowSQL = showTestSql
-	engine.ShowErr = showTestSql
-	engine.ShowWarn = showTestSql
-	engine.ShowDebug = showTestSql
+	engine.ShowSQL = mysqlShowTestSql
+	engine.ShowErr = mysqlShowTestSql
+	engine.ShowWarn = mysqlShowTestSql
+	engine.ShowDebug = mysqlShowTestSql
 
 	testAll(engine, t)
 	testAll2(engine, t)
+}
+
+func mysqlDdlImport() error {
+	engine, err := NewEngine("mysql", "root:@/?charset=utf8")
+	if err != nil {
+		return err
+	}
+	engine.ShowSQL = mysqlShowTestSql
+	engine.ShowErr = mysqlShowTestSql
+	engine.ShowWarn = mysqlShowTestSql
+	engine.ShowDebug = mysqlShowTestSql
+
+	sqlResults, _ := engine.Import("tests/mysql_ddl.sql")
+	engine.LogDebug("sql results: %v", sqlResults)
+	engine.Close()
+	return nil
 }
 
 func BenchmarkMysqlNoCache(t *testing.B) {
