64790ba262b46cc07878bf6e64845d0f25aeb2da
Nash Tsai <nash.tsai@gmail.com>
2014-08-22 17:57:17 +0800

    fixed code error

--- engine.go
+++ engine.go
@@ -38,7 +38,7 @@ type Engine struct {
 	ShowErr   bool
 	ShowDebug bool
 	ShowWarn  bool
-	// --
+	// --227
 
 	Logger     core.ILogger
 	TZLocation *time.Location
@@ -177,7 +177,7 @@ func (engine *Engine) Ping() error {
 // logging sql
 func (engine *Engine) logSQL(sqlStr string, sqlArgs ...interface{}) {
 	if engine.ShowSQL {
-		overrideLogLevel(LOG_INFO)
+		engine.overrideLogLevel(core.LOG_INFO)
 		if len(sqlArgs) > 0 {
 			engine.Logger.Info(fmt.Sprintf("[sql] %v [args] %v", sqlStr, sqlArgs))
 		} else {
@@ -188,26 +188,25 @@ func (engine *Engine) logSQL(sqlStr string, sqlArgs ...interface{}) {
 
 // logging error
 func (engine *Engine) overrideLogLevel(overrideLevel core.LogLevel) {
-		logLevel := engine.Logger.Level()
-		if logLevel == core.LOG_UNKNOWN {
-			// intend to left empty
-		}
-		else if logLevel < core.overrideLevel { // TODO can remove if deprecated engine.ShowErr
-			engine.Logger.SetLevel(LOG_ERR) // try override logger's log level
-		}
+	logLevel := engine.Logger.Level()
+	if logLevel == core.LOG_UNKNOWN {
+		// intend to left empty
+	} else if logLevel < overrideLevel { // TODO can remove if deprecated engine.ShowErr
+		engine.Logger.SetLevel(core.LOG_ERR) // try override logger's log level
+	}
 
 }
 
 func (engine *Engine) LogError(contents ...interface{}) {
 	if engine.ShowErr {
-		overrideLogLevel(LOG_ERR)
+		engine.overrideLogLevel(core.LOG_ERR)
 		engine.Logger.Err(contents...)
 	}
 }
 
 func (engine *Engine) LogErrorf(format string, contents ...interface{}) {
 	if engine.ShowErr {
-		overrideLogLevel(LOG_ERR)
+		engine.overrideLogLevel(core.LOG_ERR)
 		engine.Logger.Errf(format, contents...)
 	}
 }
@@ -215,14 +214,14 @@ func (engine *Engine) LogErrorf(format string, contents ...interface{}) {
 // logging info
 func (engine *Engine) LogInfo(contents ...interface{}) {
 	if engine.ShowInfo {
-		overrideLogLevel(LOG_INFO)
+		engine.overrideLogLevel(core.LOG_INFO)
 		engine.Logger.Info(contents...)
 	}
 }
 
 func (engine *Engine) LogInfof(format string, contents ...interface{}) {
 	if engine.ShowErr {
-		overrideLogLevel(LOG_INFO)
+		engine.overrideLogLevel(core.LOG_INFO)
 		engine.Logger.Infof(format, contents...)
 	}
 }
@@ -230,14 +229,14 @@ func (engine *Engine) LogInfof(format string, contents ...interface{}) {
 // logging debug
 func (engine *Engine) LogDebug(contents ...interface{}) {
 	if engine.ShowDebug {
-		overrideLogLevel(LOG_DEBUG)
+		engine.overrideLogLevel(core.LOG_DEBUG)
 		engine.Logger.Debug(contents...)
 	}
 }
 
 func (engine *Engine) LogDebugf(format string, contents ...interface{}) {
 	if engine.ShowDebug {
-		overrideLogLevel(LOG_DEBUG)
+		engine.overrideLogLevel(core.LOG_DEBUG)
 		engine.Logger.Debugf(format, contents...)
 	}
 }
@@ -245,14 +244,14 @@ func (engine *Engine) LogDebugf(format string, contents ...interface{}) {
 // logging warn
 func (engine *Engine) LogWarn(contents ...interface{}) {
 	if engine.ShowWarn {
-		overrideLogLevel(LOG_WARNING)
+		engine.overrideLogLevel(core.LOG_WARNING)
 		engine.Logger.Warning(contents...)
 	}
 }
 
 func (engine *Engine) LogWarnf(format string, contents ...interface{}) {
 	if engine.ShowWarn {
-		overrideLogLevel(LOG_WARNING)
+		engine.overrideLogLevel(core.LOG_WARNING)
 		engine.Logger.Warningf(format, contents...)
 	}
 }
