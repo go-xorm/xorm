a4f9a9f877e3421df5e33313473f4c0b77bbb411
Lunny Xiao <xiaolunwen@gmail.com>
2015-06-02 23:05:42 +0800

    bug fixed for #251

--- helpers.go
+++ helpers.go
@@ -134,7 +134,7 @@ func reflect2value(rawValue *reflect.Value) (str string, err error) {
 	//时间类型
 	case reflect.Struct:
 		if aa.ConvertibleTo(core.TimeType) {
-			str = rawValue.Convert(core.TimeType).Interface().(time.Time).Format(time.RFC3339Nano)
+			str = vv.Convert(core.TimeType).Interface().(time.Time).Format(time.RFC3339Nano)
 		} else {
 			err = fmt.Errorf("Unsupported struct type %v", vv.Type().Name())
 		}
