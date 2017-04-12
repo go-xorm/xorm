71e934b5c6c369ed26dedcb22bacd7338198c7d9
Lunny Xiao <xiaolunwen@gmail.com>
2017-04-12 14:35:29 +0800

    remove trace code

--- engine.go
+++ engine.go
@@ -1185,7 +1185,6 @@ func (engine *Engine) Sync(beans ...interface{}) error {
 		v := rValue(bean)
 		tableName := engine.tbName(v)
 		table, err := engine.autoMapType(v)
-		fmt.Println(v, table, err)
 		if err != nil {
 			return err
 		}
