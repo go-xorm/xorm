7b36611d51df8731d592698cce20d370e48238ff
Lunny Xiao <xiaolunwen@gmail.com>
2017-09-26 20:41:34 +0800

    simple code

--- engine_group.go
+++ engine_group.go
@@ -21,7 +21,7 @@ func NewEngineGroup(args1 interface{}, args2 interface{}, policies ...GroupPolic
 	if len(policies) > 0 {
 		eg.policy = policies[0]
 	} else {
-		eg.policy = NewRandomPolicy()
+		eg.policy = RandomPolicy()
 	}
 
 	driverName, ok1 := args1.(string)
