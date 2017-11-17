84fd68deb747faecb5d0c31febf88302b7b19921
Daniel Lee <dan.limerick@gmail.com>
2017-11-17 16:41:52 +0100

    Postgres dialect parse password with spaces (#775) (#779)
    Fixes #775 This fixes two problems - the postgres dialect can parse a
    connection string in url format that has a password with spaces. If an
    error occurs the Parse func returns an error instead of panicking.

---
+++ dialect_postgres_test.go
@@ -0,0 +1,44 @@
+package xorm
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/go-xorm/core"
+)
+
+func TestPostgresDialect(t *testing.T) {
+	TestParse(t)
+}
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		in       string
+		expected string
+		valid    bool
+	}{
+		{"postgres://auser:password@localhost:5432/db?sslmode=disable", "db", true},
+		{"postgresql://auser:password@localhost:5432/db?sslmode=disable", "db", true},
+		{"postg://auser:password@localhost:5432/db?sslmode=disable", "db", false},
+		{"postgres://auser:pass with space@localhost:5432/db?sslmode=disable", "db", true},
+		{"postgres:// auser : password@localhost:5432/db?sslmode=disable", "db", true},
+		{"postgres://%20auser%20:pass%20with%20space@localhost:5432/db?sslmode=disable", "db", true},
+		{"postgres://auser:パスワード@localhost:5432/データベース?sslmode=disable", "データベース", true},
+		{"dbname=db sslmode=disable", "db", true},
+		{"user=auser password=password dbname=db sslmode=disable", "db", true},
+		{"", "db", false},
+		{"dbname=db =disable", "db", false},
+	}
+
+	driver := core.QueryDriver("postgres")
+
+	for _, test := range tests {
+		uri, err := driver.Parse("postgres", test.in)
+
+		if err != nil && test.valid {
+			t.Errorf("%q got unexpected error: %s", test.in, err)
+		} else if err == nil && !reflect.DeepEqual(test.expected, uri.DbName) {
+			t.Errorf("%q got: %#v want: %#v", test.in, uri.DbName, test.expected)
+		}
+	}
+}
