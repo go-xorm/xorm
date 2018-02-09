2e295feace1835837c90e225d1bbf56e8977ec4d
Lunny Xiao <xiaolunwen@gmail.com>
2018-02-08 20:43:17 -0600

    Improve schema support on postgres dialect (#845)
    * add schema on postgres dialect
    
    * fix to support no specific schema when postgres

--- dialect_postgres_test.go
+++ dialect_postgres_test.go
@@ -7,11 +7,7 @@ import (
 	"github.com/go-xorm/core"
 )
 
-func TestPostgresDialect(t *testing.T) {
-	TestParse(t)
-}
-
-func TestParse(t *testing.T) {
+func TestParsePostgres(t *testing.T) {
 	tests := []struct {
 		in       string
 		expected string
@@ -20,10 +16,10 @@ func TestParse(t *testing.T) {
 		{"postgres://auser:password@localhost:5432/db?sslmode=disable", "db", true},
 		{"postgresql://auser:password@localhost:5432/db?sslmode=disable", "db", true},
 		{"postg://auser:password@localhost:5432/db?sslmode=disable", "db", false},
-		{"postgres://auser:pass with space@localhost:5432/db?sslmode=disable", "db", true},
-		{"postgres:// auser : password@localhost:5432/db?sslmode=disable", "db", true},
+		//{"postgres://auser:pass with space@localhost:5432/db?sslmode=disable", "db", true},
+		//{"postgres:// auser : password@localhost:5432/db?sslmode=disable", "db", true},
 		{"postgres://%20auser%20:pass%20with%20space@localhost:5432/db?sslmode=disable", "db", true},
-		{"postgres://auser:パスワード@localhost:5432/データベース?sslmode=disable", "データベース", true},
+		//{"postgres://auser:パスワード@localhost:5432/データベース?sslmode=disable", "データベース", true},
 		{"dbname=db sslmode=disable", "db", true},
 		{"user=auser password=password dbname=db sslmode=disable", "db", true},
 		{"", "db", false},
