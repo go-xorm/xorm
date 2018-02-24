4b595fbec32e12600c7e222756de2922704b834e
Lunny Xiao <xiaolunwen@gmail.com>
2018-02-24 16:43:28 +0800

    fix findandcount bug

--- session_find_test.go
+++ session_find_test.go
@@ -561,4 +561,11 @@ func TestFindAndCountOneFunc(t *testing.T) {
 	assert.NoError(t, err)
 	assert.EqualValues(t, 1, len(results))
 	assert.EqualValues(t, 1, cnt)
+
+	results = make([]FindAndCountStruct, 0, 1)
+	cnt, err = testEngine.Where("msg = ?", true).Select("id, content, msg").
+		Limit(1).FindAndCount(&results)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, len(results))
+	assert.EqualValues(t, 1, cnt)
 }
