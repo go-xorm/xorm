7865fcb6d8b36e393a9537a22858a9221f685c27
Lunny Xiao <xiaolunwen@gmail.com>
2018-03-26 10:05:32 +0800

    fix findandcount bug with order

--- session_find_test.go
+++ session_find_test.go
@@ -568,4 +568,11 @@ func TestFindAndCountOneFunc(t *testing.T) {
 	assert.NoError(t, err)
 	assert.EqualValues(t, 1, len(results))
 	assert.EqualValues(t, 1, cnt)
+
+	results = make([]FindAndCountStruct, 0, 1)
+	cnt, err = testEngine.Where("msg = ?", true).Desc("id").
+		Limit(1).FindAndCount(&results)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, len(results))
+	assert.EqualValues(t, 1, cnt)
 }
