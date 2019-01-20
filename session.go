6fe3ab6075e194b550aa17e0c71f5b34c87620a3
Lunny Xiao <xiaolunwen@gmail.com>
2019-01-20 12:45:38 +0800

    fix #1075 and replace #1076 (#1101)
    * fix #1075 and replace #1076
    
    * fix bug when session resue

--- session.go
+++ session.go
@@ -18,6 +18,13 @@ import (
 	"github.com/go-xorm/core"
 )
 
+type sessionType int
+
+const (
+	engineSession sessionType = iota
+	groupSession
+)
+
 // Session keep a pointer to sql.DB and provides all execution of all
 // kind of database operations.
 type Session struct {
@@ -52,8 +59,8 @@ type Session struct {
 	lastSQL     string
 	lastSQLArgs []interface{}
 
-	err error
-	ctx context.Context
+	ctx         context.Context
+	sessionType sessionType
 }
 
 // Clone copy all the session's content and return a new session
