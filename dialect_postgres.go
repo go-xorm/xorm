65e9ff78fbd23528a9fd52da9d9fe1d13f105fac
袁帆 <yuanfan@mshare.cn>
2019-05-07 14:44:59 +0800

    fix compatibility #1292

--- dialect_postgres.go
+++ dialect_postgres.go
@@ -1131,7 +1131,7 @@ func (db *postgres) GetIndexes(tableName string) (map[string]*core.Index, error)
 			return nil, err
 		}
 		indexName = strings.Trim(indexName, `" `)
-		if strings.HasSuffix(indexName, "_pkey") || strings.HasSuffix("primary") {
+		if strings.HasSuffix(indexName, "_pkey") || strings.HasSuffix(indexName, "primary") {
 			continue
 		}
 		if strings.HasPrefix(indexdef, "CREATE UNIQUE INDEX") {
