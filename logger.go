aff8ecef46b711a0ca39991b9394fe09ff35316a
XieWei <wuxiaoshen@shu.edu.cn>
2019-07-05 16:53:28 +0800

    logger color

--- logger.go
+++ logger.go
@@ -64,7 +64,7 @@ func (DiscardLogger) IsShowSQL() bool {
 	return false
 }
 
-// SimpleLogger is the default implment of core.ILogger
+// SimpleLogger is the default implement of core.ILogger
 type SimpleLogger struct {
 	DEBUG   *log.Logger
 	ERR     *log.Logger
@@ -93,6 +93,16 @@ func NewSimpleLogger3(out io.Writer, prefix string, flag int, l core.LogLevel) *
 		ERR:   log.New(out, fmt.Sprintf("%s [error] ", prefix), flag),
 		INFO:  log.New(out, fmt.Sprintf("%s [info]  ", prefix), flag),
 		WARN:  log.New(out, fmt.Sprintf("%s [warn]  ", prefix), flag),
+		level: l,
+	}
+}
+
+func NewSimpleLogger4(out io.Writer, prefix string, flag int, l core.LogLevel) *SimpleLogger {
+	return &SimpleLogger{
+		DEBUG: log.New(out, White(fmt.Sprintf("%s [debug] ", prefix)), flag),
+		ERR:   log.New(out, Red(fmt.Sprintf("%s [error] ", prefix)), flag),
+		INFO:  log.New(out, Blue(fmt.Sprintf("%s [info]  ", prefix)), flag),
+		WARN:  log.New(out, Red(fmt.Sprintf("%s [warn]  ", prefix)), flag),
 		level: l,
 	}
 }
@@ -184,4 +194,55 @@ func (s *SimpleLogger) ShowSQL(show ...bool) {
 // IsShowSQL implement core.ILogger
 func (s *SimpleLogger) IsShowSQL() bool {
 	return s.showSQL
+}
+
+// Color ...
+// Bold return a bold string
+func Bold(message string) string {
+	return fmt.Sprintf("\x1b[1m%s\x1b[21m", message)
+}
+
+// Black returns a black string
+func Black(message string) string {
+	return fmt.Sprintf("\x1b[30m%s\x1b[0m", message)
+}
+
+// White returns a white string
+func White(message string) string {
+	return fmt.Sprintf("\x1b[37m%s\x1b[0m", message)
+}
+
+// Cyan returns a cyan string
+func Cyan(message string) string {
+	return fmt.Sprintf("\x1b[36m%s\x1b[0m", message)
+}
+
+// Blue returns a blue string
+func Blue(message string) string {
+	return fmt.Sprintf("\x1b[34m%s\x1b[0m", message)
+}
+
+// Red returns a red string
+func Red(message string) string {
+	return fmt.Sprintf("\x1b[31m%s\x1b[0m", message)
+}
+
+// Green returns a green string
+func Green(message string) string {
+	return fmt.Sprintf("\x1b[32m%s\x1b[0m", message)
+}
+
+// Yellow returns a yellow string
+func Yellow(message string) string {
+	return fmt.Sprintf("\x1b[33m%s\x1b[0m", message)
+}
+
+// Gray returns a gray string
+func Gray(message string) string {
+	return fmt.Sprintf("\x1b[37m%s\x1b[0m", message)
+}
+
+// Magenta returns a magenta string
+func Magenta(message string) string {
+	return fmt.Sprintf("\x1b[35m%s\x1b[0m", message)
 }
